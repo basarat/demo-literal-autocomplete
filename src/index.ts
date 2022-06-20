4efa7a38a823fc3dfb847663624ae1c39e1150b9
basarat <basaratali@gmail.com>
2022-06-20 17:24:05 +1000

    wrap

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
-async function main() {
-  
+type Padding = 'small' | 'normal' | 'large' | string;
+
+function getPadding(padding: Padding): string {
+  if (padding === 'small') return '12px';
+  if (padding === 'normal') return '16px';
+  if (padding === 'large') return '24px';
+  return padding;
 }
 
-main();
+let padding: Padding;
+padding = 'small'; // 12px
+padding = '8px'; // 8px
